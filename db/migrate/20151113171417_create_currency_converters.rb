6612179ec573d2f74663a51a285bf79cece9c6f6
Yurii Vodolyak <yurii.vodolyak@onapp.com>
2015-11-14 12:33:59 +0200

    fix migration
    :
    Є

--- db/migrate/20151113171417_create_currency_converters.rb
+++ db/migrate/20151113171417_create_currency_converters.rb
@@ -1,10 +1,16 @@
 class CreateCurrencyConverters < ActiveRecord::Migration
-  def change
+  def up
     create_table :currencies do |t|
       t.string :currency
-      t.float :rate, :precision => 16, :scale => 2
+      t.decimal :rate, :precision => 16, :scale => 2
 
       t.timestamps
     end
+    Currency.create!(currency: "EUR", rate: 1.00)
+    Currency.create!(currency: "UAH", rate: 25.00)
+  end
+
+  def down
+    drop_table :currencies
   end
 end

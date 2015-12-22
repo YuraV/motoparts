9060a7cdd43054aaad0cf006dce5d5de4d9de977
Yurii Vodolyak <yurii.vodolyak@onapp.com>
2015-12-22 13:54:25 +0200

    brands edit destroy actions

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,12 +2,33 @@ class BrandsController < ApplicationController
   before_filter :authenticate_user!
   respond_to :html, :json
 
+  def index
+    @brands = Brand.all
+    respond_with(@brands)
+  end
+
+  def edit
+    @brand = Brand.find(params[:id])
+  end
+
+  def update
+    @brand = Brand.find(params[:id])
+    @brand.update_attributes(paramsp[:brand])
+    respond_with(@brand, location: brands_path)
+  end
+
   def new
     @brand = Brand.new
   end
 
+  def destroy
+    @brand = Brand.find(params[:id])
+    @brand.destroy
+    respond_with(@brand, location: brands_path)
+  end
+
   def create
     @brand = Brand.create(params[:brand])
-    respond_with(@brand)
+    respond_with(@brand, location: brands_path)
   end
 end

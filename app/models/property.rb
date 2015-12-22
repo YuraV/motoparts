a4aec2147c58c04f51a7ee234a6a9bb8f66690b0
Yurii Vodolyak <yurii.vodolyak@onapp.com>
2015-12-22 14:13:29 +0200

    dependent destroy for properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,10 +1,10 @@
 class Property < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :product_properties
+  has_many :product_properties, dependent: :destroy
   has_many :products, through: :product_properties
 
-  has_many :property_units
+  has_many :property_units, dependent: :destroy
   has_many :units, through: :property_units
 
   validates_presence_of :name
